6c699196499724cf2acdbb432602aefa0c7d16ce
Hermes785 <73779649+Hermes785@users.noreply.github.com>
2024-07-27 01:23:32 +0200

    Delete kibana-8.14.3 directory

--- kibana-8.14.3/src/setup_node_env/polyfill.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-require("core-js/modules/es.array.push.js");
-require("core-js/modules/es.map.group-by.js");
-require("core-js/modules/es.object.group-by.js");
-require("core-js/modules/es.promise.with-resolvers.js");
-require("core-js/modules/web.self.js");
-require("core-js/modules/web.structured-clone.js");
